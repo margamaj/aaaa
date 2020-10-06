6b9dac0f5addd30d301b8c86c8d22cb161162421
риту$$я <71869313+margamaj@users.noreply.github.com>
2020-10-06 15:42:50 +0300

    Update лаба 5, №1.cpp

--- лаба 5, №1.cpp
+++ лаба 5, №1.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int a, b, c, sum, ac, bc;
-	cout << "Введите значения точек: " << endl;
-	cout << "a: " << endl;
-	cin >> a;
-	cout << "b: " << endl;
-	cin >> b;
-	cout << "c: " << endl;
-	cin >> c;
-	ac = abs(c - a);
-	bc = abs(c - b);
-		sum = ac + bc;
-	cout << "Отрезок ас равен: " << ac << endl;
-	cout << "Отрезок bс равен: " << bc << endl;
-	cout << "Сумма отрезков равна: " << sum << endl;
+	int x1, y1, x2, y2, a;
+	cout << "Введите координаты первой точки: " << endl;
+	cout << "x1: " << endl;
+	cin >> x1;
+	cout << "y1: " << endl;
+	cin >> y1;
+	cout << "Введите координаты второй точки: " << endl;
+	cout << "x2: " << endl;
+	cin >> x2;
+	cout << "y2: " << endl;
+	cin >> y2;
+	a = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+	cout << "Расстояние между данными точками равно " << a << endl;
 	return 0;
 }
