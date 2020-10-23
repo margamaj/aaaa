1e984971c810791a87559f32b32136479ae8c601
риту$$я <71869313+margamaj@users.noreply.github.com>
2020-10-23 14:08:23 +0300

    Create лаба 7, №5.cpp

---
+++ лаба 7, №5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+float a, b, x;
+cout<<"Функция - а * х + b = 0."<<endl;
+cout<<"Введите коэффицент а: ";
+cin>>a;
+cout<<"Введите коэффицент b: ";
+cin>>b;
+x= -b / a;
+cout<<"Значение х = "<<x;
+return 0;
+}
