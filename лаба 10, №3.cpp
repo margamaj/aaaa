1e188a14c4a2d66d5ddba41cd1093acc57416a97
риту$$я <71869313+margamaj@users.noreply.github.com>
2020-10-26 21:17:58 +0300

    Create лаба 10, №3.cpp

---
+++ лаба 10, №3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Данное число является четным двузначным"<<endl;
+    cout<<"Введите a: ";
+    cin>>a;
+    
+    if (a > 9 && a < 100 && a % 2 == 0)
+    cout<<"Высказывание истинно";
+    else cout<<"Высказывание ложно";
+    return 0;
+}
