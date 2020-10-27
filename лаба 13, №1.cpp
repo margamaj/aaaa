1d0f7c89a569dc910cff3a67d2505c3699822a9a
риту$$я <71869313+margamaj@users.noreply.github.com>
2020-10-27 23:49:42 +0300

    Create лаба 13, №1.cpp

---
+++ лаба 13, №1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a;
+    cout<<"Введите цену 1 кг конфет: ";
+    cin>>a;
+    cout<<"Стоимость за:"<<endl;
+    cout<<"0.1 кг конфет: "<<a/10<<endl;
+    cout<<"0.2 кг конфет: "<<a/10*2<<endl;
+    cout<<"0.3 кг конфет: "<<a/10*3<<endl;
+    cout<<"0.4 кг конфет: "<<a/10*4<<endl;
+    cout<<"0.5 кг конфет: "<<a/10*5<<endl;
+    cout<<"0.6 кг конфет: "<<a/10*6<<endl;
+    cout<<"0.7 кг конфет: "<<a/10*7<<endl;
+    cout<<"0.8 кг конфет: "<<a/10*8<<endl;
+    cout<<"0.9 кг конфет: "<<a/10*9<<endl;
+    cout<<"1 кг конфет: "<<a<<endl;
+    return 0;
+}
