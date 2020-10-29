241b8de9159ea43c362dc334248b70d662f02da9
риту$$я <71869313+margamaj@users.noreply.github.com>
2020-10-29 21:11:15 +0300

    Create лаба 14, №2.cpp

---
+++ лаба 14, №2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cout<<"Введите а: ";
+    cin>>a;
+    cout<<"Введите b: ";
+    cin>>b;
+    while (a>b) {
+        a=a-b;
+    }
+    cout<<"Незанятая часть а: "<<a;
+    return 0;
+}
